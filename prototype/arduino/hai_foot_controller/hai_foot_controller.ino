afd60ce423f053c04e04227fd3925d56bb62786a
Haï~ <hai-vr@users.noreply.github.com>
2024-02-08 00:32:14 +0100

    cleanup inputs

--- prototype/arduino/hai_foot_controller/hai_foot_controller.ino
+++ prototype/arduino/hai_foot_controller/hai_foot_controller.ino
@@ -20,24 +20,45 @@ typedef struct __attribute__( ( packed, aligned( 1 ) ) )
 controller_buttons_t;
 
 // Make a top level struct that packs the button data along with the rest of the controller analog values
+//typedef struct __attribute__( ( packed, aligned( 1 ) ) )
+//{
+//  uint8_t       system          : 1;  //0
+//  uint8_t       a               : 1;  //1
+//  uint8_t       b               : 1;  //2
+//  uint8_t       trigger_btn     : 1;  //3
+//  uint8_t       grip            : 1;  //4
+//  uint8_t       thumbstick      : 1;  //5
+//  uint8_t       menu            : 1;  //6
+//  uint8_t       thumbstick_en   : 1;  //7
+//  uint16_t      thumbstick_x    : 10; //8
+//  uint16_t      thumbstick_y    : 10; //18
+//  uint16_t      trigger         : 10; //28
+//  uint16_t      index           : 10; //38
+//  uint16_t      middle          : 10; //48
+//  uint16_t      ring            : 10; //58
+//  uint16_t      pinky           : 10; //68
+//}
 typedef struct __attribute__( ( packed, aligned( 1 ) ) )
 {
-  uint8_t       system          : 1;  //0
-  uint8_t       a               : 1;  //1
-  uint8_t       b               : 1;  //2
-  uint8_t       trigger_btn     : 1;  //3
-  uint8_t       grip            : 1;  //4
-  uint8_t       thumbstick      : 1;  //5
-  uint8_t       menu            : 1;  //6
-  uint8_t       thumbstick_en   : 1;  //7
-  uint16_t      thumbstick_x    : 10; //8
-  uint16_t      thumbstick_y    : 10; //18
-  uint16_t      trigger         : 10; //28
-  uint16_t      index           : 10; //38
-  uint16_t      middle          : 10; //48
-  uint16_t      ring            : 10; //58
-  uint16_t      pinky           : 10; //68
-} 
+    uint8_t       system          : 1;  //0
+    uint8_t       bigtoe_is_over_secondtoe : 1;  //1
+    uint16_t      bigtoe_curl           : 10; //2
+    uint16_t      secondtoe_curl           : 10; //12
+    uint16_t      fifthtoe_splay           : 10; //22
+//    uint8_t       b               : 1;  //2
+//    uint8_t       trigger_btn     : 1;  //3
+//    uint8_t       grip            : 1;  //4
+//    uint8_t       thumbstick      : 1;  //5
+//    uint8_t       menu            : 1;  //6
+//    uint8_t       thumbstick_en   : 1;  //7
+//    uint16_t      thumbstick_x    : 10; //8
+//    uint16_t      thumbstick_y    : 10; //18
+//    uint16_t      trigger         : 10; //28
+//    uint16_t      index           : 10; //38
+//    uint16_t      middle          : 10; //48
+//    uint16_t      ring            : 10; //58
+//    uint16_t      pinky           : 10; //68
+}
 controller_data_t;
 
 // declare the controller struct for use
@@ -57,7 +78,7 @@ void setup() {
   tundra_tracker.init( );
   gpio_set_irq_enabled_with_callback( tundra_tracker.get_cs_pin(), GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &csn_irq );
 
-  controller_data.thumbstick_en = 1;
+//  controller_data.thumbstick_en = 1;
   //for demo only,
 
 //    adcAttachPin(ADC_SPLAY);
@@ -73,6 +94,7 @@ void setup() {
 float ioAval;
 float ioBval;
 float ioCval;
+float ioAcomp_prev;
 float ioCval_prev;
 
 float Comp(float input, float mul, float min, float max) {
@@ -82,6 +104,8 @@ float Comp(float input, float mul, float min, float max) {
     result = 1 - result;
     return result;
 }
+
+bool toeIsOverSecond = false;
 
 int k = 0;
 // the loop function runs over and over again forever
@@ -109,32 +133,32 @@ void loop() {
     // Simulate some changing controller data just by incrementing and toggling
     // This is the start of demo code, delete this if you do not want simulated input data
     // ----------------Begin input data simulation ----------------
-    controller_data.a = led_state;
-    controller_data.trigger +=increment;
-    
-    if ( controller_data.index <= 1000 )
-      controller_data.index+=increment;
-    else if ( controller_data.middle <= 1000 )
-      controller_data.middle+=increment;
-    else if ( controller_data.ring <= 1000 )
-      controller_data.ring+=increment;
-    else if ( controller_data.pinky <= 1000 )
-      controller_data.pinky+=increment;
-    else
-    {
-      controller_data.index = 0;
-      controller_data.middle = 0;
-      controller_data.ring = 0;
-      controller_data.pinky = 0;
-    }
-
-    if ( controller_data.thumbstick_x <= 1000 )
-      controller_data.thumbstick_x += increment;
-    else
-    {
-      controller_data.thumbstick_x = 0;    
-      controller_data.thumbstick_y += increment;       
-    }
+//    controller_data.a = led_state;
+//    controller_data.trigger +=increment;
+//
+//    if ( controller_data.index <= 1000 )
+//      controller_data.index+=increment;
+//    else if ( controller_data.middle <= 1000 )
+//      controller_data.middle+=increment;
+//    else if ( controller_data.ring <= 1000 )
+//      controller_data.ring+=increment;
+//    else if ( controller_data.pinky <= 1000 )
+//      controller_data.pinky+=increment;
+//    else
+//    {
+//      controller_data.index = 0;
+//      controller_data.middle = 0;
+//      controller_data.ring = 0;
+//      controller_data.pinky = 0;
+//    }
+//
+//    if ( controller_data.thumbstick_x <= 1000 )
+//      controller_data.thumbstick_x += increment;
+//    else
+//    {
+//      controller_data.thumbstick_x = 0;
+//      controller_data.thumbstick_y += increment;
+//    }
 
     // ----------------End of input data simulation ----------------
 
@@ -169,18 +193,28 @@ void loop() {
    float ioAcomp = Comp(ioAval, fx, 0.5, 1.0f); // 0.75 = 0.45 * (1 / 0.6f)
    float ioBcomp = Comp(ioBval, fx, 0.5, 1.0f);
    float ioCcomp = Comp(ioCval, fx, 0.5, 1.0f);
-      if (ioAval > 1) ioAval = 1;
-      if (ioBval > 1) ioBval = 1;
-      if (ioCval > 1) ioCval = 1;
-      controller_data.index = ioAcomp * 1023;
-      controller_data.middle = ioBcomp * 1023;
-      controller_data.ring = ioCcomp * 1023;
-      controller_data.pinky = 1023;
+      controller_data.bigtoe_curl = ioAcomp * 1023;
+      controller_data.secondtoe_curl = ioBcomp * 1023;
+      controller_data.fifthtoe_splay = ioCcomp * 1023;
 
-      if (ioCval != ioCval_prev) {
-        ioCval_prev = ioCval;
-          controller_data.grip = !controller_data.grip;
-      }
+
+if (!toeIsOverSecond) {
+    if (ioAcomp_prev <= 0.25 && ioAcomp > 0.25 && ioBcomp > 0.25) {
+        toeIsOverSecond = true;
+    }
+}
+else {
+    if (ioAcomp < 0.15) toeIsOverSecond = false;
+    else if (ioBcomp < 0.15) toeIsOverSecond = false;
+}
+    controller_data.bigtoe_is_over_secondtoe = toeIsOverSecond ? 1 : 0;
+    controller_data.system = 0;
+    ioAcomp_prev = ioAcomp;
+
+//      if (ioCval != ioCval_prev) {
+//        ioCval_prev = ioCval;
+//          controller_data.grip = !controller_data.grip;
+//      }
 }
 
 // Callback for SPI Chipselect, just connect in the tmi irq function
